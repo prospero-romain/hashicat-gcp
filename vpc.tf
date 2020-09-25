36f8fa84ead5c6138c292cbb04480b5c2c716f42
prospero-romain <contact@chevalier-prospero.fr>
2020-09-25 11:38:08 +0000

    add vpc.tf file

--- vpc.tf
+++ vpc.tf
@@ -1,29 +1,33 @@
-module "test-vpc-module" {
-  source       = "../../"
-  project_id   = var.project_id
-  network_name = network_name
+module "vpc" {
+    source  = "terraform-google-modules/network/google"
+    version = "~> 2.5"
 
-  subnets = [
-    {
-      subnet_name   = "${local.subnet_01}"
-      subnet_ip     = "10.10.10.0/24"
-      subnet_region = "us-west1"
-    },
-    {
-      subnet_name           = "${local.subnet_02}"
-      subnet_ip             = "10.10.20.0/24"
-      subnet_region         = "us-west1"
-      subnet_private_access = "true"
-      subnet_flow_logs      = "true"
-    },
-    {
-      subnet_name               = "${local.subnet_03}"
-      subnet_ip                 = "10.10.30.0/24"
-      subnet_region             = "us-west1"
-      subnet_flow_logs          = "true"
-      subnet_flow_logs_interval = "INTERVAL_10_MIN"
-      subnet_flow_logs_sampling = 0.7
-      subnet_flow_logs_metadata = "INCLUDE_ALL_METADATA"
-    }
-  ]
+    project_id   = "<PROJECT ID>"
+    network_name = "example-vpc"
+    routing_mode = "GLOBAL"
+
+    subnets = [
+        {
+            subnet_name           = "subnet-01"
+            subnet_ip             = "10.10.10.0/24"
+            subnet_region         = "us-west1"
+        },
+        {
+            subnet_name           = "subnet-02"
+            subnet_ip             = "10.10.20.0/24"
+            subnet_region         = "us-west1"
+            subnet_private_access = "true"
+            subnet_flow_logs      = "true"
+            description           = "This subnet has a description"
+        },
+        {
+            subnet_name               = "subnet-03"
+            subnet_ip                 = "10.10.30.0/24"
+            subnet_region             = "us-west1"
+            subnet_flow_logs          = "true"
+            subnet_flow_logs_interval = "INTERVAL_10_MIN"
+            subnet_flow_logs_sampling = 0.7
+            subnet_flow_logs_metadata = "INCLUDE_ALL_METADATA"
+        }
+    ]
 }
